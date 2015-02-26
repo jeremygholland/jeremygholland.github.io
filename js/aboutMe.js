b7c702fb0b6b5ee7eda454a2669e49588746e66b
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-02-25 22:42:04 -0600

    second commit

---
+++ js/aboutMe.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('#resumeStuff').hide();
+	$('#projectStuff').hide();
+	$('#resume').click(function(){
+		$('#resume').toggleClass('woo');
+		$('#projects').removeClass('woo');
+		$('#resumeStuff').toggle('fast');
+		if(!$('#resume').hasClass('woo')){
+			$('#resumeStuff').hide();
+				}
+		if(!$('#projects').hasClass('woo')){
+			$('#projectStuff').hide();
+		}
+		
+	});
+	$('#projects').click(function(){
+		$('#projects').toggleClass('woo');
+		$('#resume').removeClass('woo');
+		$('#projectStuff').toggle('fast');
+		if(!$('#projects').hasClass('woo')){
+			$('#projectStuff').hide();
+		}
+		if(!$('#resume').hasClass('woo')){
+			$('#resumeStuff').hide();
+		}
+	})
+});
