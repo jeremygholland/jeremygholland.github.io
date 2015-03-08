17a0e09911a11a2d83b07207a2dc42ab058c334e
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-03-07 18:00:23 -0600

    3/7/2015 commit

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$('#resumeStuff').hide();
 	$('#projectStuff').hide();
 	$('#resume').click(function(){
+		 new WOW().init();
 		$('#resume').toggleClass('woo');
 		$('#projects').removeClass('woo');
 		$('#resumeStuff').toggle('fast');
@@ -13,13 +14,33 @@ $(document).ready(function(){
 		}
 		
 	});
+
+	var thisFunction = function(){
+if ($(window).width() >750){
+				var wow3 = ".wow pulse infinite";
+			var wow = ".wow fadeInRight";
+			var wow1 = ".wow fadeInLeft"; 
+			console.log($(window).width());
+			$('#first').removeClass(wow1);
+			$('#second').removeClass(wow);
+			$('#third').removeClass(wow1);
+			$('#fourth').removeClass(wow);
+			$('#first, #second, #third, #fourth').hover(function(){
+				$(this).addClass(wow3);
+			},function(){
+				$(this).removeClass(wow3);
+			})
+		};
+	}
 	$('#projects').click(function(){
+		 new WOW().init();
+		 thisFunction();
 		$('#projects').toggleClass('woo');
 		$('#resume').removeClass('woo');
 		$('#projectStuff').toggle('fast');
 		if(!$('#projects').hasClass('woo')){
 			$('#projectStuff').hide();
-		}
+					}
 		if(!$('#resume').hasClass('woo')){
 			$('#resumeStuff').hide();
 		}
