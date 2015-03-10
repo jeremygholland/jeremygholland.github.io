94ce5a46824601d7bac85f4d2e3e68dc37b609cf
Jeremy Holland <jeremyholland@Jeremys-MacBook-Pro.local>
2015-03-09 19:58:54 -0500

    03/09/2015 new commit

--- js/aboutMe.js
+++ js/aboutMe.js
@@ -52,9 +52,4 @@ $(document).ready(function(){
 			$('#projectStuff').hide();
 		}
 	})
-	$('.skillbar').each(function(){
-		$(this).find('.skillbar-bar').animate({
-			width:$(this).attr('data-percent')
-		},6000);
-	});
 });
